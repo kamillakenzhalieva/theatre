07b3f7f64e5257542ce18858e930d715f1e8c8c8
kamillakenzhalieva <kamillakenzhalieva@gmail.com>
2026-04-04 12:35:06 +0400

    страница день рождения, заглушки на бд пока что

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('afisha/', views.afisha, name='afisha'),
-    path('about/', views.about, name='about'), 
+    path('about/', views.about, name='about'),
+    path('birthdays/', views.birthdays, name='birthdays'),
 ]
--- main/views.py
+++ main/views.py
@@ -8,5 +8,39 @@ def about(request):
     return render(request, 'about.html')
 
 def afisha(request):
-    events = Event.objects.all()
-    return render(request, 'afisha.html', {'events': events})
+    mock_events = [
+        {
+            'title': 'Три поросенка',
+            'date': '12 апреля, 12:00',
+            'location': 'Большой зал',
+            'description': 'Классическая сказка в интерактивном формате. Малыши помогут строить домики и спасаться от волка!',
+            'price': '800',
+            'ticket_link': '#'
+        },
+        {
+            'title': 'Тайна старого шкафа',
+            'date': '19 апреля, 16:00',
+            'location': 'Камерная сцена',
+            'description': 'Волшебная история о дружбе и храбрости. Зрителей ждут световые эффекты и чудеса.',
+            'price': '1200',
+            'ticket_link': '#'
+        }
+    ]
+    return render(request, 'afisha.html', {'events': mock_events})
+
+def birthdays(request):
+    mock_packages = [
+        {
+            'title': 'Пакет «Стандарт»',
+            'description': 'Аренда зала на 2 часа, интерактивная сказка, поздравление от героя и чаепитие.',
+            'price': '15 000',
+            'features': ['До 10 детей', '1 аниматор', 'Базовый декор']
+        },
+        {
+            'title': 'Пакет «Премиум»',
+            'description': 'Аренда на 3 часа, расширенный спектакль, шоу мыльных пузырей и тематическая фотозона.',
+            'price': '25 000',
+            'features': ['До 20 детей', '2 аниматора', 'Шоу-программа']
+        }
+    ]
+    return render(request, 'birthdays.html', {'packages': mock_packages})
